6fafeaa2c39ff7a77c2358bbcf96b9d67d62e882
spyros zaboukis <spyros.zaboukis.83@gmail.com>
2022-11-15 20:28:53 +0100

    send styled-component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,27 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Colors from '../../utils/style/Colors'
+
+const StyledLink = styled(Link)`
+padding: 15px;
+color: #8186a0;
+text-decoration: none;
+font-size: 18px;
+${(props) => 
+    props.$isFullLink &&
+    `color: white; border-radius: 30px; background-color: ${Colors.primary};`
+}`
+
+
  
 function Header() {
+
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/Survey/08">Questionnaire</Link>
-            <Link to="/Freelances">Freelances</Link>
+            <StyledLink to="/">Home</StyledLink>
+            <StyledLink to="/Survey/08">Questionnaire</StyledLink>
+            <StyledLink to="/Freelances">Freelances</StyledLink>
+            <StyledLink to="/Freelances2" $isFullLink>Freelances2</StyledLink>
         </nav>
     )
 }
--- src/index.js
+++ src/index.js
@@ -8,24 +8,33 @@ import Error from './components/Error/index.jsx';
 import Result from './pages/Result/index.jsx';
 import Freelances from './pages/Freelances/index.jsx';
 
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
 
     <BrowserRouter>
-    
+    <GlobalStyle/>
       <Header/>
+        <Routes> 
+          <Route path="/" element={ <Home/> } />
+          <Route path="/Result" element={<Result/>} />
+          <Route path="Survey/:questionNumber" element={ <Survey/> } />
+          <Route path='/Freelances' element={<Freelances/>}/>
+          <Route path="*" element={ <Error/> } />
 
-      <Routes> 
-        <Route path="/" element={ <Home/> } />
-        <Route path="/Result" element={<Result/>} />
-        <Route path="Survey/:questionNumber" element={ <Survey/> } />
-        <Route path='/Freelances' element={<Freelances/>}/>
-        <Route path="*" element={ <Error/> } />
-
-      </Routes>
+        </Routes>
       
     </BrowserRouter>
     </React.StrictMode>
 );
+
+
